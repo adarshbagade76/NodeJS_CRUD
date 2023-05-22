fd09eccd3083634ff0009fea28d994f602313a26
Adarsh M Bagade <adarsh.bagade@ratnaglobaltech.com>
2023-05-22 16:58:32 +0530

    crud Operation

---
+++ Start.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const app=express()
+app.use(express.json())
+const users=[]
+app.get('/get',(req,res)=>{
+res.send(users)
+})
+app.post('/create',(req,res)=>{
+    const user=req.body
+    users.push(user)
+res.send(user)
+
+})
+
+app.put('/', () => {
+    res.send('Started')
+})
+app.listen(3000,()=>{
+    console.log("app start")
+})
